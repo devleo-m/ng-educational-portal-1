df2d19029c6f8201cadbca7cecb81c8f5bca93b1
Leonardo Madeira <dev.leomadeira@gmail.com>
2023-12-21 22:26:06 -0300

    criando pagina login/email

---
+++ dotnet-squadManager/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_squadManager.Models
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+        public string Email { get; set; }   
+    }
+}
