e292ebab673c6ab29232c9e5ebc0b18dea5f0ac0
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-07-26 02:43:33 -0300

    remove

--- dotnet-squadManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dotnet_squadManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- dotnet-squadManager/Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dotnet_squadManager.Models
-{
-    public class UserViewModel
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }    
-        public string Email { get; set; }   
-    }
-}
